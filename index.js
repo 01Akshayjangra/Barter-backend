46eabac324d2dcd2783dcb4bc1b15749f8f2fc98
Akshay kumar <akshayjangra225@gmail.com>
2023-04-14 11:53:13 +0530

    set up cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const cors = require('cors');
 dotenv.config({path:'./config.env'})
 require('./db/conn')
 app.use(express.json());
-app.use(require('./router/user-routes'))
-app.use(require('./router/post-routes'))
+app.use(cors());
+
+app.use(cors(), require('./router/user-routes'))
+app.use(cors(), require('./router/post-routes'))
 
 const port = process.env.PORT || 5000;   
 app.listen(port,()=>{
