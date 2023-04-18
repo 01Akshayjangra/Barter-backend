8b031135506610151bdeffdeffdc1f54c23c5c6e
engineers_bro <kartikchauhan336@gmail.com>
2023-04-19 01:41:56 +0530

    fix post issue

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+var cookieParser = require('cookie-parser');
 const cors = require('cors');
 dotenv.config({path:'./config.env'})
 require('./db/conn')
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use(cors(), require('./router/user-routes'))
 app.use(cors(), require('./router/post-routes'))
--- router/post-routes.js
+++ router/post-routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const verifyToken  = require('../middleware/auth')
 const {getAllPosts , createPost, getUserPosts} = require('../Controllers/post-controller')
 const router = express.Router();
 
 // router.get('/post',getPost)
 // router.get('/posts/:postId', getPost);
 
-router.get('/posts/user/:userId', getUserPosts);
-router.get('/posts', getAllPosts);
-router.post('/post',createPost)
+router.get('/posts/user/',verifyToken, getUserPosts);
+router.get('/post', getAllPosts);
+router.post('/post',verifyToken,createPost)
 
 module.exports = router
