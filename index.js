031898f98cd8a78a7c0f9b40fe696090b2942964
Akshay kumar <akshayjangra225@gmail.com>
2023-05-04 21:20:23 +0530

    whitelist

--- index.js
+++ index.js
@@ -13,22 +13,31 @@ const app = express();
 
 const path = require("path");
 
-dotenv.config({path:'./config.env'})
+dotenv.config({ path: './config.env' })
 require("./config/db");
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-  }));
+const whitelist = ['https://barterr.vercel.app'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json()); //to accept json data
 app.use(cookieParser());
-  
+
 app.use("/api/user", userRoutes);
 app.use("/api", postRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 
-const port = process.env.PORT || 5000;   
-app.listen(port,()=>{
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
     console.log(`The server is running at localhost:${port}`)
 })
