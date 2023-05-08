bc760bb95721a4ac644f4368b74e22957d06dc93
Akshay kumar <akshayjangra225@gmail.com>
2023-05-08 08:38:43 +0530

    hosting

--- index.js
+++ index.js
@@ -9,26 +9,24 @@ const messageRoutes = require("./routes/messageRoutes");
 
 var cookieParser = require('cookie-parser');
 const app = express();
-// const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const path = require("path");
 
 dotenv.config({ path: './config.env' })
 require("./config/db");
 
-// const whitelist = ['https://barterr.vercel.app'];
-// const whitelist = ['https://localhost:3000'];
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// };
+const whitelist = ['https://barterr.vercel.app'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
 
-// app.use(cors(corsOptions)); 
+app.use(cors(corsOptions)); 
 app.use(express.json()); //to accept json data
 app.use(cookieParser());
 
