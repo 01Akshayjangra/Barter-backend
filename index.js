73ead210ece37c9ab529647cc13fa5bd8705b4a1
Akshay kumar <akshayjangra225@gmail.com>
2023-05-12 21:27:22 +0530

    for hosting

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ const path = require("path");
 dotenv.config({ path: './config.env' })
 require("./config/db");
 
-// const whitelist = ['https://barterr.vercel.app'];
+const whitelist = ['https://barterr.vercel.app'];
 // const whitelist = ['https://localhost:3000'];
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// };
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json()); //to accept json data
 app.use(cookieParser());
 
